b4e8a20ddf8229d49588239454cfbd72930e7b77
Eomsigi <eomsigi0218@gmail.com>
2025-09-24 21:09:43 +0900

    feat: test app add + deploy workflow test

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello AWS! 배포 테스트 성공 🎉");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
